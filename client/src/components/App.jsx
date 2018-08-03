62c6a4ebbf149a2b22a320cf230d9e344298deb4
Whitney Seiler <whitney.seiler@gmail.com>
2018-08-03 12:15:26 -0700

    set state using form input

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,7 +8,12 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      owner: '',
+      repo: '',
+      event: ''
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -23,17 +28,27 @@ class App extends React.Component {
     // });
   }
 
+  handleChange(event) {
+    let id = event.target.id;
+    let value = event.target.value;
+  
+    this.setState({
+      [id] : value
+    });
+  }
+
   onSubmit(event) {
     event.preventDefault();
 
-    var form = document.getElementsByClassName(form);
+    var form = document.getElementsByClassName('form');
 
     for (var i = 0; i < form.length; ++i) {
       let field = form[i].id;
       let value = form[i].value;
+      console.log(field, ' : ', value)
       this.setState({
         [field]: value
-      });
+      }, console.log(this.state));
     } 
   }
 
@@ -41,7 +56,13 @@ class App extends React.Component {
     return (
       <div>
         <h1>GitHub Events API Fetcher</h1>
-        <FormOne />
+        <FormOne 
+          onSubmit={this.onSubmit} 
+          handleChange={this.handleChange} 
+          owner={this.state.owner} 
+          repo={this.state.repo} 
+          event={this.state.event}
+        />
         <ResultsContainer />
       </div>
     )
--- client/src/components/FormOne.jsx
+++ client/src/components/FormOne.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Row, Input} from 'react-materialize'
+import {Row, Input, Button} from 'react-materialize'
 
-let FormOne = ({onSubmit, event}) => {
+let FormOne = ({handleChange, onSubmit, owner, repo, event, e}) => {
     return (
         <div>
-            <h3>Please Enter Your Contact Information:</h3>
+            <h4>Please Enter Your Repo Information:</h4>
             <Row>
-                <Input type="text" id="owner" className="f1" label="Owner:"/>
-                <Input type="text" id="repo" className="f1" label="Repo:"/>
-                <Input type="text" id="event" className="f1" label="Event:"/>
-                <Input type="submit" onClick={(event) => onSubmit(event)} value="DISPLAY"/>
+                <Input type="text" id="owner" label="Owner:" onChange={(e) => handleChange(e)}/>
+                <Input type="text" id="repo" label="Repo:" onChange={(e) => handleChange(e)}/>
+                <Input type="text" id="event" label="Event:" onChange={(e) => handleChange(e)}/>
+                <Button waves="light" onClick={(e) => onSubmit(e)} id="display">DISPLAY</Button>
             </Row>
         </div>
     )
